ec9fbb20a2345b15dc1fd9e4bf9c8253781b423f
Jackson Zhu <zhujackson1115@gmail.com>
2024-10-21 21:44:19 -0400

    Adjusted map size

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -143,7 +143,7 @@ const MapComponent = ({ locations, nearbyLocations = [] }) => {
                 </label>
             </div>
 
-            <MapContainer center={[40.7128, -74.0060]} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+            <MapContainer center={[40.7128, -74.0060]} zoom={13} style={{ height: '75vh', width: '100vw' }}>
                 {/* Add OpenStreetMap tile layer */}
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
