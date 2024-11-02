d737040830c1a946b745a695f301803b12bbbbbf
Jackson Zhu <zhujackson1115@gmail.com>
2024-11-02 10:58:16 -0400

    Initial Commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,12 +17,36 @@ function Home() {
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const [userCoord, setUserCoord] = useState({ lat: null, lon:  null});
+    const [startCoord, setStartCoord] = useState({ lat: null, lon:  null});
     const [destination, setDestination] = useState({ lat: null, lon:  null});
     
     //user selected locations
     const handleSearch = (searchTerm) => {
         setSelectedLocation(searchTerm);
     };
+
+    useEffect(() => {
+        if(params.get('lat') && params.get('lon') && params.get('preflat') && params.get('preflon')){
+            setDestination({lat: params.get('lat'), lon: params.get('lon')});
+            setStartCoord({lat: params.get('preflat'), lon: params.get('preflon')})
+        }
+        else if(params.get('lat') && params.get('lon')){
+            setDestination({lat: params.get('lat'), lon: params.get('lon')});
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const { latitude, longitude } = position.coords;
+                        setStartCoord({lat: latitude, lon: longitude});
+                    },
+                    (error) => {
+                        console.error('Geolocation error:', error);
+                    }
+                );
+            } else {
+                alert('Geolocation is not supported by this browser.');
+            }
+        }
+    }, [location.search]);
 
     useEffect(() => {
         if (effectRan.current) return;
@@ -32,10 +56,6 @@ function Home() {
         if (token) {
             const decodedToken = JSON.parse(atob(token.split('.')[1]));
             setName(decodedToken.name);
-        }
-
-        if(params.get('lat') && params.get('lon')){
-            setDestination({lat: params.get('lat'), lon: params.get('lon')});
         }
 
         // Fetch all locations
@@ -59,7 +79,6 @@ function Home() {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     const { latitude, longitude } = position.coords;
-                    setUserCoord({lat: latitude, lon: longitude});
                     console.log('User Coordinates: ', { latitude, longitude });
 
                     fetch(`${import.meta.env.VITE_PORT}/locations/nearby?lat=${latitude}&lon=${longitude}`)
@@ -106,7 +125,7 @@ function Home() {
                 locations={locations} 
                 nearbyLocations={nearbyLocations} 
                 selectedLocation={selectedLocation}
-                userCoord = {userCoord}
+                userCoord = {startCoord}
                 destination={destination}
             />
 
